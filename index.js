ed54ac987f706f0df05d9017ab5cedce001b3936
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-06 19:15:52 +0300

    commit tree 586eebac524264136cc52ff47d3a77956eb87f84

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+let num1= 2, num2= 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() *10);
+let num3=4, num4= 9;
+let mod =num3 % num4;
+let max= Math.max(4,10,20,6,7,3);
